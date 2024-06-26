80b16f36e6ecf4368e23bf842b073da1f3fd2d18
Shisha1536 <schischkoff2017@yandex.ru>
2024-06-26 14:07:10 +0300

    +

---
+++ src/pages/components/layout.tsx
@@ -0,0 +1,26 @@
+import { PropsWithChildren } from "react";
+import Head from "next/head";
+import Navigation from "./navigation";
+
+
+export default function Layout({ children }: PropsWithChildren) {
+    return (
+    <>
+        <Head>
+        <title>Web Studio</title>
+        <meta name="description" content="SkillFactory Next.js project" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        </Head>
+    
+        <div>
+        <header>
+            <div>Logo</div>
+            <Navigation />
+        </header>
+        <main>{children}</main>
+        <footer>&copy; 2023 Web studio</footer>
+        </div>
+    </>
+    );
+}
---
+++ src/pages/components/navigation.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function Navigation() {
+    return (
+      <nav>
+        <Link href="/">
+          Главная
+        </Link>
+        <Link href="/about">
+          О компании
+        </Link>
+        <Link href="/contacts">
+          Контакты
+        </Link>
+      </nav>
+    );
+}
